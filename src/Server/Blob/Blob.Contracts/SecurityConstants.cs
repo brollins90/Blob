138e5f9574482061c4c687ae5978316857e5dd77
Blake Rollins <brollins90@gmail.com>
2015-05-19 11:49:39 -0600

    A

---
+++ src/Server/Blob/Blob.Contracts/SecurityConstants.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Blob.Contracts
+{
+    public static class SecurityConstants
+    {
+        public const string IdentityProviderClaimType = "http://schemas.microsoft.com/accesscontrolservice/2010/07/claims/identityprovider";
+        public const string NameIdentifier = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier";
+        public const string IdentityProviderClaimValue = "BlobClaims";
+        public const string SecurityStampClaimType = "BlobClaims.SecurityStamp";
+
+        public const string BlobIdClaimType = "BlobIdClaim";
+        public const string BlobNameClaimType = "BlobNameClaim";
+        public const string BlobPrincipalTypeClaimType = "BlobPrincipalTypeClaim";
+        public const string BlobRoleClaimType = "BlobRoleClaim";
+        public const string CustomerIdClaimType = "CustomerIdClaim";
+        public const string DeviceTypeClaimType = "DeviceTypeClaim";
+        public const string UserTypeClaimType = "UserTypeClaim";
+    }
+}
