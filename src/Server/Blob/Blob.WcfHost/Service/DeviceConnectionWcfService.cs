90a00012ec03a5a0c9f18893d21a408f25dbc48c
brollins90 <brollins90@gmail.com>
2015-11-04 16:04:42 -0700

    moved device connection service to the wcf host project

---
+++ src/Server/Blob/Blob.WcfHost/Service/DeviceConnectionWcfService.cs
@@ -0,0 +1,42 @@
+﻿using Blob.Contracts.ServiceContracts;
+using Blob.Services;
+using System;
+using System.IdentityModel.Services;
+using System.Security.Permissions;
+using System.ServiceModel;
+
+namespace Blob.WcfHost.Service
+{
+    [ServiceBehavior]
+    [GlobalErrorBehavior(typeof(GlobalErrorHandler))]
+    public class DeviceConnectionWcfService : IDeviceConnectionService
+    {
+        IDeviceConnectionService _deviceConnectionService;
+
+        public DeviceConnectionWcfService(IDeviceConnectionService deviceConnectionService)
+        {
+            _deviceConnectionService = deviceConnectionService;
+        }
+
+        [OperationBehavior]
+        [ClaimsPrincipalPermission(SecurityAction.Demand, Resource = "device", Operation = "connect")]
+        public void Connect(Guid deviceId)
+        {
+            _deviceConnectionService.Connect(deviceId);
+        }
+
+        [OperationBehavior]
+        [ClaimsPrincipalPermission(SecurityAction.Demand, Resource = "device", Operation = "connect")]
+        public void Disconnect(Guid deviceId)
+        {
+            _deviceConnectionService.Disconnect(deviceId);
+        }
+
+        [OperationBehavior]
+        [ClaimsPrincipalPermission(SecurityAction.Demand, Resource = "device", Operation = "connect")]
+        public void Ping(Guid deviceId)
+        {
+            _deviceConnectionService.Ping(deviceId);
+        }
+    }
+}
