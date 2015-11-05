740fa490a56cb26a099d1c97ccf95ffb9c4e8a5c
brollins90 <brollins90@gmail.com>
2015-11-04 17:41:03 -0700

    moved the wcf attributes to the wcf host

---
+++ src/Server/Blob/Blob.WcfHost/GlobalErrorBehaviorAttribute.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+
+namespace Blob.WcfHost
+{
+    // http://www.codeproject.com/Articles/801898/WCF-Global-Exception-Handling-using-IErrorHandler
+
+    public class GlobalErrorBehaviorAttribute : Attribute, IServiceBehavior
+    {
+        private readonly Type _errorHandlerType;
+
+        public GlobalErrorBehaviorAttribute(Type errorHandlerType)
+        {
+            _errorHandlerType = errorHandlerType;
+        }
+
+        #region IServiceBehavior Members
+
+        void IServiceBehavior.Validate(ServiceDescription description, ServiceHostBase serviceHostBase)
+        {
+        }
+
+        void IServiceBehavior.AddBindingParameters(ServiceDescription description, ServiceHostBase serviceHostBase, System.Collections.ObjectModel.Collection<ServiceEndpoint> endpoints, BindingParameterCollection parameters)
+        {
+        }
+
+        void IServiceBehavior.ApplyDispatchBehavior(ServiceDescription description, ServiceHostBase serviceHostBase)
+        {
+            IErrorHandler errorHandler;
+
+            try
+            {
+                errorHandler = (IErrorHandler)Activator.CreateInstance(_errorHandlerType);
+            }
+            catch (MissingMethodException e)
+            {
+                throw new ArgumentException("The errorHandlerType specified in the ErrorBehaviorAttribute constructor must have a public empty constructor.", e);
+            }
+            catch (InvalidCastException e)
+            {
+                throw new ArgumentException("The errorHandlerType specified in the ErrorBehaviorAttribute constructor must implement System.ServiceModel.Dispatcher.IErrorHandler.", e);
+            }
+
+            foreach (ChannelDispatcherBase channelDispatcherBase in serviceHostBase.ChannelDispatchers)
+            {
+                ChannelDispatcher channelDispatcher = channelDispatcherBase as ChannelDispatcher;
+                channelDispatcher.ErrorHandlers.Add(errorHandler);
+            }
+        }
+
+        #endregion  IServiceBehavior Members
+    }
+}
